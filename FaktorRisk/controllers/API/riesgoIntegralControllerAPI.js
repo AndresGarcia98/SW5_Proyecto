82444267c098e0d106dd76ce7614c39c30e27220
Javier Ramirez <jav7123@hotmail.com>
2020-11-10 19:02:54 -0500

    Subo busqueda por procesos

--- FaktorRisk/controllers/API/riesgoIntegralControllerAPI.js
+++ FaktorRisk/controllers/API/riesgoIntegralControllerAPI.js
@@ -17,6 +17,45 @@ exports.map_general_risk = function(req, res) {
     }
 }
 
-exports.gerential_inform = function(req, res) {
+exports.processes_inform = function(req, res) {
+    var num_doc = Riesgo.countDocuments({}, (err, result) => {
+        if (err) return "Lo sentimos ha surgido un problema y no podemos realizar tu petición";
+        else return result;
+    });
+    if (num_doc != "Lo sentimos ha surgido un problema y no podemos realizar tu petición") {
+        Riesgo.aggregate(
+            [{$match: {
+                "id_user": req.body.id_user
+              }}, {$unwind: {
+                path: '$proceso_asociado'
+              }}, {$group: {
+                _id: "$proceso_asociado",
+                datos_riesgo: {
+                  $push: {"code": "$code",
+                  "nombre": "$riesgo",
+                  "descripcion":"$descripcion",
+                  "controles_tratamientos":"$controles_tratamientos",
+                  "riesgo_asociado":"$riesgo_asociado",
+                  "causas": "$causas",
+                  "frecuencia":"$frecuencia",
+                  "formula_indicador":"$formula_indicador",
+                  "medicion_inherente": "$medicion_inherente",
+                  "medicion_residual": "$medicion_residual",
+                          }
+                }
+              }}],
+            (err, data) => {
+                if (err) res.status(500).json({
+                    mensaje: "Lo sentimos  cliente " + req.body.id_user + " ha surgido un problema y no podemos mostrar el informe por procesos de riesgo integral," +
+                        "puede ser un problema nuestro o revisa que ingresaste un ID de usuario. Disculpa por las molestias "
+                });
+                else res.send(data)
+            });
+    } else {
+        res.json({
+            mensaje: num_doc
+        });
+    }
+    
 
 }
